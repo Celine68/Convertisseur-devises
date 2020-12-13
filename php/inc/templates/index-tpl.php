c15e972542559939261f5da90b4917e77494dd0f
celine <celinecetre@gmail.com>
2020-12-13 11:48:59 +0100

    Intégration css

--- php/inc/templates/index-tpl.php
+++ php/inc/templates/index-tpl.php
@@ -7,22 +7,27 @@
     <title>Convertisseur de devises</title>
 </head>
 <body>
-    <main>
+    <header>
         <h1>Convertisseur de devises</h1>
+    </header>
 
+    <main>
         <form method="post" action="./index.php">
-            <input name="value1" type="number" value="<?=$valueToConvert?>"></input>
-            <select name="currency1">
+        
+            <input class="form_value-to-convert form_input" name="value1" type="number" value="<?=$valueToConvert?>"></input>
+            <select class="form_currency-to-convert form_select" name="currency1">
                 <option value='<?=$currentCurrency?>'><?=$currencies[$currentCurrency]['name']?></option>
                 <?php foreach ($currencies as $currency => $datas) {
-                  if ($currency != $currentCurrency) { ?>
+                    if ($currency != $currentCurrency) { ?>
                 <option value=<?=$currency?>><?=$datas['name']?></option>
                 <?php }
                 } ?>
             </select>
-            <span> = </span>
-            <input name="value2" type="number" value="<?=$valueConverted?>" disabled></input>
-            <select name="currency2">
+            
+            <div class="form_equal"> = </div>
+            
+            <input class="form_converted-value form_input" name="value2" type="number" value="<?=$valueConverted?>" disabled></input>
+            <select class="form_converted-currency form_select" name="currency2">
                 <option value='<?=$currencyToConvert?>'><?=$currencies[$currencyToConvert]['name']?></option>
                 <?php foreach ($currencies as $currency => $datas) { 
                     if ($currency != $currencyToConvert) { ?>
@@ -30,7 +35,9 @@
                 <?php }
                 } ?>
             </select>
+            
             <button>Convertir</button>
+        
         </form>
     </main>
 </body>
